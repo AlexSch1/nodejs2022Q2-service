e04d33d2393040e0662c2dae1a0b56d3c42c89e9
Aleksey Shulyak <a.shulyak@itransition.com>
2022-07-14 13:37:50 +0400

    feature: Initial structure

---
+++ src/core/db.ts
@@ -0,0 +1,73 @@
+import {CreateUserDto} from "../features/users/dto/create-user.dto";
+import {IUser} from "../shared/interfaces/user";
+import {v4} from "uuid";
+
+export const USERS_TABLE = 'users';
+
+type tableNames = typeof USERS_TABLE;
+type tableTypes = IUser;
+
+interface MyDb {
+	users: IUser[];
+}
+
+export class InMemoryDB implements MyDb {
+	[USERS_TABLE]: IUser[] = [{
+		id: '2b6febbf-fcf1-412b-be44-9008c14fa694',
+		login: 'login',
+		password: '123',
+		version: 1,
+		createdAt: Date.now(),
+		updatedAt: Date.now(),
+	}];
+
+	async getAllEntities<T extends tableNames>(
+		tableName: T
+	): Promise<tableTypes[]> {
+		return this[tableName];
+	}
+
+	async getEntity<T extends tableNames>(
+		tableName: T,
+		idEntity: string
+	): Promise<tableTypes | undefined> {
+		return this[tableName].find(({ id }) => id === idEntity);
+	}
+
+	async createEntity<T extends tableNames>(
+		tableName: T,
+		item: tableTypes
+	): Promise<void> {
+		this[tableName].push(item);
+	}
+
+	async updateEntity<T extends tableNames>(
+		tableName: T,
+		user: tableTypes
+	): Promise<tableTypes> {
+		this[tableName] = this[tableName].map((dbUser) => {
+			if (dbUser.id === user.id) {
+				return {
+					...dbUser,
+					...user,
+				};
+			}
+
+			return dbUser;
+		});
+
+		return user;
+	}
+
+	async removeEntity<T extends tableNames>(
+		tableName: T,
+		idEntity: string
+	): Promise<void> {
+		this[tableName] = this[tableName].filter((dbUser) => dbUser.id !== idEntity);
+
+	}
+}
+
+const db = new InMemoryDB()
+
+export default db;
