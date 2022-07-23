52f51ad5ce1663ba8a8a19d1fef717d17b28a1bd
Aleksey Shulyak <a.shulyak@itransition.com>
2022-07-23 20:33:37 +0400

    feature: refactor

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import {join} from "path";
-import {config} from "dotenv";
-
-config({
-  path: join(__dirname, '../../.env'),
-});
+import {ENV} from "./core/config";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(ENV.PORT);
 }
 bootstrap();
