d80144c75ac56319b8dc95f02a274fb71b203297
Aleksey Shulyak <a.shulyak@itransition.com>
2022-08-07 21:02:14 +0400

    feature: refactor

--- src/core/http-exception-filter.ts
+++ src/core/http-exception-filter.ts
@@ -11,13 +11,12 @@ export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
     response.status(status).json({
       message: exception.getResponse(),
       statusCode: status || 500,
-      path: request.url,
+      path: ctx.getRequest<Request>().url,
     });
   }
 }
