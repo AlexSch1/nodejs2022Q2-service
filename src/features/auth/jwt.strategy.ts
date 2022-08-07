23bdacf4f0591bb26d509e2e3d9c886319787885
Aleksey Shulyak <a.shulyak@itransition.com>
2022-08-07 18:00:08 +0400

    feature: add jwt

---
+++ src/features/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth.service';
+import {ENV} from "../../core/config";
+import {LoginDto} from "./dto/login.dto";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: ENV.JWT_SECRET_KEY,
+    });
+  }
+
+  async validate(payload: {
+    login: string;
+    userId: string;
+  }): Promise<LoginDto> {
+    const user = await this.authService.validate(payload.userId);
+
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+}
